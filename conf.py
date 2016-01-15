77fda6b76304a10b3a09228d8fca0919d0aea127
Eric Ramseur <ramseur@anant.us>
2016-01-15 13:12:42 -0500

    add conf.py to build with

---
+++ conf.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#
+# -- General configuration -----------------------------------------------------
+
+source_suffix = '.rst'
+master_doc = 'index'
+
+project = u'Learn Appleseed'
+copyright = u'2008-2016, ANANT Corporation'
+
+version = '1.0.7.0'
+
+# -- Options for HTML output ---------------------------------------------------
+
+ 
+
+# -- HTML theme options for `dotted` style -------------------------------------
+
+ 
